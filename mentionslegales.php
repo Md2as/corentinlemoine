242d28a74a41f14111646b5cbd21c123ba89a354
CORENTIN LEMOINE <c.lemoine@cs2i-lorient.fr>
2021-04-21 17:55:09 +0200

    first commit old website

---
+++ mentionslegales.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Corentin LEMOINE - Mentions légales">
+    <meta name="author" content="Corentin LEMOINE">
+    <meta name="robots" content="noindex, nofollow">
+    <title>Corentin LEMOINE - Mentions légales</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha256-LA89z+k9fjgMKQ/kq4OO2Mrf8VltYml/VES+Rg0fh20=" crossorigin="anonymous" />
+  </head>
+  <body>
+    <main role="main" class="container">
+      <h1 class="mt-5">Mentions légales</h1>
+      <div class="card">
+        <div class="card-header">Hébergement</div>
+        <div class="card-body">
+            <!--sse-->
+            <p>Wdes SAS</p>
+            <p>SIRET : 829 562 883 00019, R.C.S. LORIENT</p>
+            <p>Société par Actions Simplifiée Unipersonelle (SASU) au capital de 500 €</p>
+            <p>Adresse : 5 impasse Bob Gestin, 56110 Gourin, France</p>
+            <p><a target="_blank" href="https://www.infogreffe.fr/entreprise-societe/829562883">Greffe du Tribunal de Commerce de LORIENT</a></p>
+            <p>Email de contact : <a href="mailto:williamdes@wdes.fr">williamdes@wdes.fr</a></p>
+            <!--/sse-->
+        </div>
+      </div>
+     <div class="card mt-5 mb-5">
+        <div class="card-header">Directeur de la publication et éditeur du site</div>
+        <div class="card-body">
+            <!--sse-->
+            <p>M. Corentin LEMOINE</p>
+            <p>Email de contact : <a href="mailto:corentin.devweb@gmail.com">corentin.devweb@gmail.com</a></p>
+            <!--/sse-->
+        </div>
+        <div class="card-footer"><a href="/">retour à l'accueil</a></div>
+      </div>
+    </main>
+  </body>
+</html>
